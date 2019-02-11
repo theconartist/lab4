d2f18a155e5dd3fdbcee175fcc02d145ff191fc8
Michael Gale <m.gale@warwick.ac.uk>
2019-02-11 08:21:29 +0000

    Add solutions

--- src/Lab4.hs
+++ src/Lab4.hs
@@ -8,41 +8,57 @@ module Lab4 where
 --------------------------------------------------------------------------------
 -- Red-black trees
 
-data Colour = ???
+data Colour = Red | Black
 
 instance Show Colour where
-    show = undefined
+    show Red   = "Red"
+    show Black = "Black"
 
-data Tree a = Leaf | ???
+data Tree a = Leaf | Node Colour (Tree a) a (Tree a)
     deriving Show
 
 empty :: Tree a
-empty = undefined
+empty = Leaf
 
 singleton :: a -> Tree a
-singleton = undefined
+singleton x = Node Red empty x empty
 
 makeBlack :: Tree a -> Tree a
-makeBlack = undefined
+makeBlack (Node _ l x r) = Node Black l x r
+makeBlack tree           = tree
 
 depth :: Tree a -> Int
-depth = undefined
+depth Leaf           = 0
+depth (Node _ l _ r) = 1 + max (depth l) (depth r)
 
 toList :: Tree a -> [a]
-toList = undefined
+toList Leaf           = []
+toList (Node _ l x r) = toList l ++ [x] ++ toList r
 
 member :: Ord a => a -> Tree a -> Bool
-member = undefined
+member _ Leaf = False
+member x (Node _ l y r)
+    | x == y    = True
+    | x < y     = member x l
+    | otherwise = member x r
 
 balance :: Colour -> Tree a -> a -> Tree a -> Tree a
 balance Black (Node Red (Node Red a x b) y c) z d =
     Node Red (Node Black a x b) y (Node Black c z d)
-balance _ _ _ _ = undefined
-balance _ _ _ _ = undefined
-balance _ _ _ _ = undefined
+balance Black (Node Red a x (Node Red b y c)) z d =
+    Node Red (Node Black a x b) y (Node Black c z d)
+balance Black a x (Node Red (Node Red b y c) z d) =
+    Node Red (Node Black a x b) y (Node Black c z d)
+balance Black a x (Node Red b y (Node Red c z d)) =
+    Node Red (Node Black a x b) y (Node Black c z d)
 balance c l x r = Node c l x r
 
 insert :: Ord a => Tree a -> a -> Tree a
-insert = undefined
+insert tree x = makeBlack (ins tree)
+    where ins Leaf = singleton x
+          ins (Node c l y r)
+            | x<y       = balance c (ins l) y r
+            | y<x       = balance c l y (ins r)
+            | otherwise = Node c l y r
 
 --------------------------------------------------------------------------------
